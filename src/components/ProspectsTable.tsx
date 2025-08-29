31ea1d1110800223245cbb4d1b91b1f8eba798e0
Yggdrazzil <98061760+Yggdrazzil@users.noreply.github.com>
2025-08-29 11:22:01 +0200

    Updated ProspectsTable.tsx

--- src/components/ProspectsTable.tsx
+++ src/components/ProspectsTable.tsx
@@ -400,7 +400,6 @@ export function ProspectsTable({
         >
           <table className="w-full border-collapse">
             <thead className="bg-white dark:bg-gray-800 sticky top-0 z-10 border-b border-gray-200 dark:border-gray-700">
-            <thead className="sticky-header border-b border-gray-200 dark:border-gray-700">
               <tr className="text-left">
                 <th className="p-2 sm:p-4 w-16 bg-white dark:bg-gray-800" />
                 <th 
